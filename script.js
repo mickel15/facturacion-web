47b07b625788afe2144cef7509de7ce86a018623
mickel15 <mikel.guerra68@gmail.com>
2026-05-11 12:01:35 -0500

    nueva conexion

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // URL de tu Apps Script
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwRMilvmynnQ-xK-JX36kBAGz5IDIKMkdfBMdb0k4eRmMYr9ThXaKxCYGUnqa4LeLmR/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyz-6gON9iQrUoDI_HVMKwQ6McQ--49lNMYeIIpTFdnTkmnmRiL04-7HrZc8vvpiEEG/exec";
 
 const btn = document.getElementById('btnGenerar');
 const resultadosDiv = document.getElementById('resultados');
